8c3d4f556690515810886571a2d2f3eb20971b89
Sean Stockholm <sstockholm@roomkey.com>
2018-10-30 19:34:06 -0400

    remove sort file and make some changes

--- src/sorting/core.clj
+++ src/sorting/core.clj
@@ -1,46 +1,51 @@
 (ns sorting.core
   (:gen-class)
-  (:require [sorting.suffix-tree :as tree]
+  (:require #_[sorting.suffix-tree :as tree]
             [clojure.java.io :as io]))
+
+(defn extract [d] (nth data (nth d 3)))
+
+(defn lrmu-sort [coll]
+  (let [compares [- - compare (fn [idx-1 idx-2] (compare (nth coll idx-1) (nth coll idx-2)))]]
+    (map extract
+         (sort (fn [xx yy] (or (some #(when (not= 0 %) %)
+                                     (map #(%1 %2 %3) compares xx yy))
+                               0))
+               (map-indexed (fn [idx s] (m-lrmus s idx)) coll)))))
 
 (defn -main
   [input output]
   (let [to-sort (with-open [rdr (clojure.java.io/reader input)]
                   (doall (line-seq rdr)))]
-    (my-sort to-sort)
+    (lrmu-sort to-sort)
     (def start (System/currentTimeMillis))
-    (def sorted (my-sort to-sort))
-    (println (- end (System/currentTimeMillis)))
+    (def sorted (lrmu-sort to-sort))
+    (def end (System/currentTimeMillis))
+    (println (- start end))
     (dorun (map #(spit output % :append true) sorted))))
 
 ;; load everything in order skipping -main
 
-(defn lrmu-sort [coll]
-  (let [compares [- - compare (fn [idx-1 idx-2] (compare (nth coll idx-1) (nth coll idx-2)))]]
-    (sort (fn [xx yy] (or (some #(when (not= 0 %) %)
-                                (map #(%1 %2 %3) compares xx yy))
-                          0))
-          (map-indexed (fn [idx s] (tree/m-lrmus s idx)) coll))))
 
-(def data (with-open [rdr (clojure.java.io/reader (io/resource "data1.txt"))]
+
+#_(def data (with-open [rdr (clojure.java.io/reader (io/resource "data1.txt"))]
             (doall (line-seq rdr))))
-
-(defn extract [d] (nth data (nth d 3)))
 
 ;; We are shooting for 3 seconds ... or at least less than 6.
 ;; On my macbook, peter's code took 3 seconds.
-(time (def sorted (doall (map extract (lrmu-sort data)))))
+
+#_(time (def sorted (doall (map extract (lrmu-sort data)))))
 
 ;; sorted data to compare to
-(def sorted-data (with-open [rdr (clojure.java.io/reader (io/resource "out1.txt"))]
+#_(def sorted-data (with-open [rdr (clojure.java.io/reader (io/resource "out1.txt"))]
                    (doall (line-seq rdr))))
 
-(= sorted sorted-data)
+#_(= sorted sorted-data)
 
 #_(def parsed (time (doall (map-indexed (fn [idx s] (tree/m-lrmus s idx)) data))))
 
 ;; Run me
-(time (spit "out1.txt" (str (clojure.string/join "\n" sorted) "\n")))
+#_(time (spit "out1.txt" (str (clojure.string/join "\n" sorted) "\n")))
 ;; Then
 ;; diff out1.txt resources/out1.txt
 ;; should be clear
